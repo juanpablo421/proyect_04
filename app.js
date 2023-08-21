ca48139ced2a345726f0d2b40923fc33ea9afcea
25JuanP <jb061366@gmail.com>
2023-08-20 23:49:43 -0500

    proyect_04

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ console.clear();
 //console.log(Math);
 
 const main = async() => {
-  let x=  (Math.getRandomInt(100))
-  let y= (Math.getRandomInt(50))
-  console.log('****************************************'.bgYellow);
-    console.log('****'.blue,'FUNCIONES MATEMATICAS'.bgMagenta.white,
-                '*************'.blue);
-    console.log('****************************************\n'.yellow);
+  let x=  (Math.getRandomInt(1000))
+  let y= (Math.getRandomInt(5000))
+  console.log('****************************************'.bgBlack);
+    console.log('****'.bgRed,'FUNCIONES MATEMATICAS'.bgMagenta.white,
+                '*************'.bgRed);
+    console.log('****************************************\n'.bgBlack);
   console.log("NUMEROS GENERADOS =>"+" "+x+","+ y);
-     console.log('****************************************'.white.bgBlue);
-     console.log("//".white.bgBlue +"     SUMA =" + Math.add(x,y))
-     console.log("//".white.bgBlue +"    RESTA =" + Math.substract(x,y)) 
-     console.log("//".white.bgBlue +"    MULTIPLICACION =" + Math.multiply(x,y))
-     console.log("//".white.bgBlue +"    DIVISION =" + Math.divide(x,y))
-     console.log('****************************************'.white.bgBlue);
+     console.log('****************************************'.bgRed);
+     console.log("//".bgRed +"     SUMA =" + Math.add(x,y))
+     console.log("//".bgRed +"    RESTA =" + Math.substract(x,y)) 
+     console.log("//".bgRed +"    MULTIPLICACION =" + Math.multiply(x,y))
+     console.log("//".bgRed +"    DIVISION =" + Math.divide(x,y))
+     console.log('****************************************'.bgRed);
      
 
 
--- modules/math.js
+++ modules/math.js
@@ -15,9 +15,9 @@ const Maths = {}
         // define la funcion dividir(divide)
      function divide(a,b){
         if ((b ==0 && a==0)||(b==0)){
-            return `ERROR0:N/A :: VALORES a= ${a} y b =${b}`.bgRed
+            return `ERROR0:N/A :: VALORES a= ${a} y b =${b}`.red.bgYellow
             }else if (a== 0) {
-                return `ERROR0:N/A :: VALORES a= ${a} y b =${b}`.bgRed
+                return `ERROR0:N/A :: VALORES a= ${a} y b =${b}`.red.bgYellow
             }else{
                 return a / b;
 
